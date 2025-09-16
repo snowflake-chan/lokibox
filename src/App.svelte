d8f432ebb467315927538df6687b8af5e601d9cc
snowflake-chan <lawrence.bi@icloud.com>
2025-09-16 21:27:31 +0800

    清空App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,8 @@
 <script lang="ts">
-  const register: string[] = ["JetPack", "AutoAim", "AutoClick"];
 </script>
 
 <main>
-  <div class="bar-container">
-    {#each register as i}
-      <div class="bar-item">{i}</div>
-    {/each}
-  </div>
 </main>
 
 <style lang="scss">
-  .bar-container {
-  position: absolute;
-  bottom:80px;
-  right: 0;
-  padding: 0.5em;
-  display: flex;
-  flex-direction: column;
-  }
-
-.bar-item {
-  text-align: right;
-  background: linear-gradient(
-    90deg,
-    #ff0000,
-    #ff8000,
-    #ffff00,
-    #00ff00,
-    #00ffff,
-    #0000ff,
-    #ff00ff,
-    #ff0000
-  );
-  background-size: 200% 100%; /* 双倍宽度用于动画 */
-  background-clip: text;
-  -webkit-background-clip: text;
-  color: transparent;
-  font-weight: bold;
-  font-size: 1em;
-  animation: rainbowFlow 4s ease-in-out infinite alternate;
-}
-
-@keyframes rainbowFlow {
-  0% { background-position: 0% 50%; }
-  100% { background-position: 100% 50%; }
-}
 </style>
